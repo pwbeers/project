f347dc5e98674b48250da1c5cd5001bfca69b141
StephanMB <stephan.braams@gmail.com>
2015-01-16 12:04:38 +0100

    Added Javadoc, comments and method stubs to Error

--- Server/Server/Error.java
+++ Server/Server/Error.java
@@ -1,11 +1,40 @@
 package Server;
 
 /**
- * Extends Exception
+ * Handles the exceptions and errors from all classes in the server
+ * @author Stephan *
  */
-public class Error {
+public class Error extends Exception{
 	// ------------------ Instance variables ----------------
+	private String errorMesssage; //A message to be displayed or send to a clien
 	//------------------ Constructor ------------------------
+	/**
+	 * Sets up the variables
+	 */
+	public Error(){
+		// TODO - implement Error.Error
+		throw new UnsupportedOperationException();
+	}
 	// ------------------ Queries --------------------------
 	// ------------------ Commands --------------------------
+	/**
+	 * Send an AMULET-ERROR message to the client, with an explanation in errorMessage
+	 * @param errorMessage the message to be sent to the client
+	 */
+	public void clientError(String errorMessage){
+		// TODO - implement Error.clientError
+		throw new UnsupportedOperationException();
+		
+	}
+	
+	/**
+	 * Prints an error message in the server GUI
+	 * @param errorMessage the message to be displayed
+	 */
+	public void serverError(String errorMessage){
+		// TODO - implement Error.serverError
+		throw new UnsupportedOperationException();		
+	}
+	
+	
 }
