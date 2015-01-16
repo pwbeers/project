096feede4d947b42ccbf1763a48c26f1d27db78d
Peter Beers <pwbeers@gmail.com>
2015-01-16 12:08:40 +0100

    JML toegevoegd aan Game en Game Model laten implementeren

--- Model/Model/Game.java
+++ Model/Model/Game.java
@@ -7,7 +7,7 @@ import java.util.Observable;
  * This class keeps track of a game from start till finish.
  * @author peter
  */
-public class Game extends Observable	{
+public class Game extends Observable implements Model	{
 
 	private /*@ spec_public @*/ Board board;
 	private /*@ spec_public @*/ List<Integer> players;
@@ -55,6 +55,22 @@ public class Game extends Observable	{
 	 */
 	//@ requires isWinner(players.get(0)) || isWinner(players.get(1));
 	public void endGame() {
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * Places a stone in the given column for the given player if the move is legal
+	 * and the player is on turn. Then checks if the board has a winner,draw, or no winner.
+	 * If 0 is returned then the player is not on turn or the player made an illegal move.
+	 * If 1 is returned then the player has won.
+	 * If 2 is returned then there is no winner.
+	 * @param column <code>=> 0 && column <= 6</code>
+	 * @param player <code>== 1 || player == 2</code>
+	 */
+	//@ requires column >= 0 && column <= 6;
+	//@ requires player == 1 || player == 2;
+	//@ ensures \result == 0 || \result == 1 || \result == 2;
+	public int doMove(int column, int player) {
 		throw new UnsupportedOperationException();
 	}
 
