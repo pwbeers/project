1947db541e837c59188184c7e09e29cab4b397c1
StephanMB <stephan.braams@gmail.com>
2015-01-20 17:13:57 +0100

    Started implementing ServerGui

--- Server/View/ServerGui.java
+++ Server/View/ServerGui.java
@@ -3,6 +3,32 @@ package View;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.SystemColor;
+import java.util.Observable;
+
+import javax.swing.ImageIcon;
+
+import java.awt.event.ActionListener;
+import java.awt.GridLayout;
+
+import javax.swing.UIManager;
+import javax.swing.JToggleButton;
+import javax.swing.JSlider;
 
 /**
  * A ServerGui that shows a graphical interface for the Server/user to use.
@@ -11,27 +37,13 @@ import javax.swing.JFrame;
 public class ServerGui implements View {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ServerGui window = new ServerGui();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private ActionListener controller;
 
 	/**
 	 * Create the application.
 	 */
-	public ServerGui() {
+	public ServerGui(ActionListener newController) {
+		controller = newController;
 		initialize();
 	}
 
@@ -43,5 +55,14 @@ public class ServerGui implements View {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+	
+	public void printTekst(String message){
+		throw new UnsupportedOperationException();		
+	}
+
+	public void startScherm(){
+		throw new UnsupportedOperationException();
+	}
+
 
 }
