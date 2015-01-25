406d3712249d8e12eaebd563ca283cf2acb35ebe
StephanMB <stephan.braams@gmail.com>
2015-01-25 15:13:13 +0100

    Added starter to server and stub for GUI

---
+++ Server/Server/Starter.java
@@ -0,0 +1,13 @@
+package Server;
+
+import View.ServerGui;
+import java.awt.event.ActionListener;
+
+public class Starter {
+	public static void main(String[] args)	{
+		ServerController arg = new ServerController(2200);
+		new ServerGui(arg);
+	}
+
+
+}
--- Server/View/ServerGui.java
+++ Server/View/ServerGui.java
@@ -23,12 +23,15 @@ import java.util.Observable;
 
 import javax.swing.ImageIcon;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.GridLayout;
 
 import javax.swing.UIManager;
 import javax.swing.JToggleButton;
 import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.BoxLayout;
 
 /**
  * A ServerGui that shows a graphical interface for the Server/user to use.
@@ -38,6 +41,8 @@ public class ServerGui implements View {
 
 	private JFrame frame;
 	private ActionListener controller;
+	private JTextField textField;
+	private JTextArea textArea;
 
 	/**
 	 * Create the application.
@@ -54,15 +59,43 @@ public class ServerGui implements View {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textArea = new JTextArea();
+		textArea.setBounds(0, 0, 434, 199);
+		frame.getContentPane().add(textArea);
+		
+		JButton btnNewButton = new JButton("Send");
+		btnNewButton.setBounds(335, 210, 89, 41);
+		frame.getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 220, 314, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String Line = textField.getText();
+				printTekst(Line);
+			}
+		});
+
 	}
 	
 	public void printTekst(String message){
-		throw new UnsupportedOperationException();		
+		textArea.append(message);		
 	}
 
 	public void startScherm(){
 		throw new UnsupportedOperationException();
 	}
+	
+	public void update(Observable arg0, Object arg1) {
+		throw new UnsupportedOperationException();
+	}
 
-
+	public  void main(String[] args){
+		initialize();
+	}
 }
